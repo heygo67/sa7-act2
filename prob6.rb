05685aee17d1e4350fddd857a6e4ecd7fdaf5a75
McKenna Noakes <mckenna.noakes@outlook.com>
2024-02-16 23:58:25 -0600

    prob6 completed

---
+++ prob6.rb
@@ -0,0 +1,5 @@
+student = {name: "Alice", age: 20, grade: "A"}
+
+puts "The student's name is #{student[:name]}."
+puts "The student's age is #{student[:age]}."
+puts "The student's grade is #{student[:grade]}."
