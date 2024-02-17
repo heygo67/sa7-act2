9f1934de3c7a450dfb134046e27a6c6716a24ba2
McKenna Noakes <mckenna.noakes@outlook.com>
2024-02-16 23:50:36 -0600

    prob1 completed

---
+++ prob1.rb
@@ -0,0 +1,14 @@
+module HTMLGenerator
+    def tag(name, content)
+      "<#{name}>#{content}</#{name}>"
+    end
+  end
+  
+  include HTMLGenerator
+  
+  paragraph = tag :p, "This is a paragraph"
+  header = tag :h1, "This is a header"
+  
+  puts paragraph
+  puts header
+  
