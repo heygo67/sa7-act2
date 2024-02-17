695f34d1f722a8a5c6ac47fba000675ef496f090
McKenna Noakes <mckenna.noakes@outlook.com>
2024-02-16 23:54:47 -0600

    prob4 completed

---
+++ prob4.rb
@@ -0,0 +1,17 @@
+input_file = 'input.txt'
+output_file = 'output.txt'
+
+begin
+  content = File.read(input_file)
+
+  reversed_content = content.reverse
+
+  File.open(output_file, 'w') do |file|
+    file.write(reversed_content)
+  end
+
+  puts "Successfully reversed content and wrote to #{output_file}"
+
+rescue Errno::ENOENT
+  puts "Error: File '#{input_file}' not found."
+end
